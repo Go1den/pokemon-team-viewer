790a1f98c4ee1836d7a32eec6b889de2a7f7cae4
Go1den <gooneden@gmail.com>
2026-07-18 22:15:08 -0500

    Flip

--- js/main.js
+++ js/main.js
@@ -9,15 +9,28 @@ window.addEventListener("load", (event) => {
 });
 
 function refresh(num) {
-    let name = document.getElementById("input" + num).value;
-    name = name.toLowerCase();
+    let dexNum = document.getElementById("input" + num).value;
     let el = document.getElementById("pokemon" + num);
-    let imgSource = "https://www.serebii.net/pokemonhome/pokemon/" + name + ".png";
-    el.innerHTML = '<img src="' + imgSource + '" />';
+    if (dexNum < 10) {
+        dexNum = "00" + dexNum;
+    } else if (dexNum < 100) {
+        dexNum = "0" + dexNum;
+    }
+    let imgSource = "https://www.serebii.net/pokemonhome/pokemon/" + dexNum + ".png";
+    el.innerHTML = '<img id="image' + num + '" src="' + imgSource + '" />';
 }
 
 function setBG() {
     let name = document.getElementById("color1").value;
     console.log(name);
     document.getElementById("table1").style.backgroundColor = name;
+}
+
+function flip(num) {
+    let el = document.getElementById("image" + num);
+    if (el.classList.contains('flipped')) {
+        el.classList.remove('flipped');
+    } else {
+        el.classList.add('flipped');
+    }
 }
